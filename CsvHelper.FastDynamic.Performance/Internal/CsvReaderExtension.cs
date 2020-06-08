417bd51eb631246c61fec7157f3855fde5fd0089
Tatsuro Shibamura <me@shibayan.jp>
2020-06-08 23:13:52 +0900

    Adding benchmark project

---
+++ CsvHelper.FastDynamic.Performance/Internal/CsvReaderExtension.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CsvHelper.FastDynamic.Performance.Internal
+{
+    internal static class CsvReaderExtension
+    {
+        internal static IList<IDictionary<string, object>> GetDictionaryRecords(this CsvReader csvReader)
+        {
+            // Read Header
+            csvReader.Read();
+            csvReader.ReadHeader();
+
+            var headerRecord = csvReader.Context
+                                        .HeaderRecord
+                                        .Select((x, i) => csvReader.Configuration.PrepareHeaderForMatch(x, i))
+                                        .ToArray();
+
+            var result = new List<IDictionary<string, object>>();
+
+            while (csvReader.Read())
+            {
+                var record = new Dictionary<string, object>(headerRecord.Length);
+
+                for (int i = 0; i < headerRecord.Length; i++)
+                {
+                    record[headerRecord[i]] = csvReader[i];
+                }
+
+                result.Add(record);
+            }
+
+            return result;
+        }
+    }
+}
